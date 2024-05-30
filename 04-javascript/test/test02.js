a58d89d1c5c9da5b306da53ce5c67182c13b2252
DAIN302 <dbgnfk12@gmail.com>
2024-05-30 10:46:01 +0900

    실습 내용 변경

--- 04-javascript/test/test02.js
+++ 04-javascript/test/test02.js
@@ -11,25 +11,22 @@ const square = (x) => console.log(x**2)
 // square(11)
 // square(5)
 
-function ageGroup(x){
-    if(x >= 20) {
+let age = prompt('나이를 입력해주세요')
+
+function ageGroup(age){
+    if(age >= 20) {
         return "성인"
-    } else if( x >= 17) {
+    } else if(age >= 17) {
         return "고등학생"
-    } else if( x >= 14) {
+    } else if(age >= 14) {
         return "중학생"
-    } else if( x >= 8) {
+    } else if(age >= 8) {
         return "초등학생"
-    } else if(x >= 0) {
+    } else if(age >= 0) {
         return "유아"
     } else {
         return "0이상 입력해주세요"
     }
 }
 
-console.log(ageGroup(24));
-console.log(ageGroup(17));
-console.log(ageGroup(15));
-console.log(ageGroup(11));
-console.log(ageGroup(4));
-console.log(ageGroup(-4));
+console.log(ageGroup(age));
