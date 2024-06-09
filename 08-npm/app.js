dc0293626b5a548cff1f0932053e5f22db14a28c
DAIN302 <dbgnfk12@gmail.com>
2024-06-10 00:04:29 +0900

    Update app.js

--- 08-npm/app.js
+++ 08-npm/app.js
@@ -9,6 +9,7 @@ http.createServer(function(req, res){ // req - request(요청) , res - response(
     // test/html -> html로 해석해라
     res.writeHead(200, {'Content-Type' : 'text/html'}) 
     // 응답(res) 이거 주고 끝내(end)
+    res.write(`<h1>Hello</h1>`)
     res.end(`<h2>Hello World</h2>`)
 }).listen(8080, ()=>{
     console.log('8080포트에서 실행 중');
