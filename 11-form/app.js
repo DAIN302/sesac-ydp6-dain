c6cdffca2b6da144a797b2030f20a08f10579c27
DAIN302 <dbgnfk12@gmail.com>
2024-06-14 18:45:38 +0900

    stduy : form

--- 11-form/app.js
+++ 11-form/app.js
@@ -26,22 +26,33 @@ app.get('/', (req, res)=>{
 })
 
 app.get('/getForm', (req, res)=>{
-    // console.log(req.query); // { id: 'dsd', pwd: 'sdd' }
-    res.send('get 요청 성공')
+    // console.log(req.query); // { id: 'banana', pw: '1234' }
+    // get 방식으로 응답을 요청시 req.query 의 결과가 url에 보여짐 http://localhost:8888/getForm?id=banana&pw=1234
+    // res.send('get 요청 성공')
+    res.render('result', {
+        title : 'GET 요청 결과',
+        userInfo : req.query // req.query { id: 'banana', pw: '1234' }
+    })
 })
 
 app.post('/postForm', (req, res)=>{
     // console.log(req.query); // {}
-    console.log(req.body); 
+    // console.log(req.body); 
     // undefined -> body-parser 미들웨어 함수를 쓰지 않아서 
-    // { id: 'ㅁㄹㅁㄹㄴㅇ', pwd: 'ㄴㅇfsdf' } -> body-parser 사용해서 데이터를 받았음
-    res.send('post 요청 성공')
+    // { id: 'apple', pwd: '1234 } -> body-parser 사용해서 데이터를 받았음
+    // res.send('post 요청 성공')
+    res.render('result', {
+        title : 'POST 요청 결과', 
+        userInfo : req.body // req.body { id: 'apple', pw: '1234' }
+    })
 })
 
 // res.send() : 문자열, JSON, 파일 등을 클라이언트에게 응답 (JSON을 가장 많이 응답)
 // -> 서버가 데이터를 응답할 때 사용
-// res.render() : 템플릿 엔진을 사용해서 서버 측에서 "동적으로" HTML을 생성하고 클라이언트에게 응답
+// res.render(view, data) : 템플릿 엔진을 사용해서 서버 측에서 "동적으로" HTML을 생성하고 클라이언트에게 응답
 // -> 서버 측에서 동적으로 페이지를 렌더링할 때 사용
+// -> view : views/ 폴더의 ejs 파일 연결
+// -> data :  view에 넣어줄 정보
 
 // listen(port, callback) : server start method
 app.listen(PORT, ()=>{
