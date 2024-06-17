e6e2c82ff86562e3109f2f422ca2aadcc8c675ce
DAIN302 <dbgnfk12@gmail.com>
2024-06-17 17:18:32 +0900

    study :axios & fetch

--- 12-form-dynamic/index.js
+++ 12-form-dynamic/index.js
@@ -19,13 +19,34 @@ app.get('/', (req, res)=>{
 // ajax 요청 처리 : get
 app.get('/ajax', (req, res)=>{
     // get방식이므로 브라우저에서 url 쿼리까지 직접 입력해서도 값 확인 가능
-    console.log(req.query);
+    // console.log(req.query);
     res.send(req.query)
 })
 
 // ajax 요청 처리 : post
 app.post('/ajax', (req, res)=>{
-    console.log(req.body);
+    //console.log(req.body);
+    res.send(req.body)
+})
+
+// axios 요청 처리 : get
+app.get('/axios', (req,res)=>{
+    //console.log(req.query);
+    res.send(req.query)
+})
+
+// axios 요청 처리 : post
+app.post('/axios', (req,res)=>{
+    res.send(req.body)
+})
+
+// fetch 요청 처리 : get
+app.get('/fetch', (req,res)=>{
+    res.send(req.query)
+})
+
+// fetch 요청 처리 : post
+app.post('/fetch', (req,res)=>{
     res.send(req.body)
 })
 
