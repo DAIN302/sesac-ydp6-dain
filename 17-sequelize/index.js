d1dc43f5e17698629482f38337f3be7094b26971
DAIN302 <dbgnfk12@gmail.com>
2024-06-28 16:09:14 +0900

    study : sequelize

---
+++ 17-sequelize/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+const router = require('./routes/index');
+const { sequelize } = require('./models/index')
+
+// 뷰 템플릿
+app.set('view engine', 'ejs');
+app.set('views', './views'); 
+
+//  body-parser 미들웨어 등록
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+
+// 라우터 등록
+app.use('/', router);
+
+// 모델 등록
+sequelize.sync({force:false}) // force : true -> 서버 실행때마다 테이블 재생성 / false -> 서버 실행 시 테이블이 없으면 생성
+.then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`database connection succeeded`);
+        console.log(`Server is running! Port number is ${PORT}`);
+    });
+}).catch((err)=>console.log(err))
+
---
+++ 17-sequelize/models/index.js
@@ -0,0 +1,53 @@
+const Sequelize = require('sequelize'); // sequelize 패키지 등록
+const config = require(__dirname + '/../config/config.json')["development"]; // json파일의 development키의 값들에 접근(db연결 정보)
+const db = {}; // 빈 객체
+
+// sequelize 객체 생성
+const  sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+// 모델 불러오기
+const PlayerModel = require('./Player')(sequelize, Sequelize); // playerModel 함수를 불러온 후 매개변수 sequelize, Sequelize를 전달한 것
+const ProfileModel = require('./Profile')(sequelize, Sequelize); // playerModel 함수를 불러온 후 매개변수 sequelize, Sequelize를 전달한 것
+const TeamModel = require('./Team')(sequelize, Sequelize); // playerModel 함수를 불러온 후 매개변수 sequelize, Sequelize를 전달한 것
+
+// 모델간 관계 연결
+// 1) Player : Profile = 1 : 1
+// 하나의 선수당 하나의 프로필을 가짐
+PlayerModel.hasOne(ProfileModel, { 
+  // cascade 옵션 : player 가 삭제/수정 되는 경우 profile 도 함께 삭제/업데이트
+  onDelete: 'CASCADE', 
+  onUpdate: 'CASCADE',
+  // ProfileModel 에 'player_id' 외래키 생성 
+  foreignKey: 'player_id',
+  // PlayerModel의 'player_id' 컬럼 참조
+  sourceKey: 'player_id'
+});
+ProfileModel.belongsTo(PlayerModel, {
+  // ProfileModel 에 'player_id' 외래키 생성 
+  foreignKey: 'player_id',
+  // 참조하게 될 키는 'player_id'
+  targetKey: 'player_id'
+})
+
+// 2) Team : Player = 1: N
+// 한 팀에는 여러 명의 선수가 존재
+TeamModel.hasMany(PlayerModel, {
+  foreignKey: 'team_id',
+  sourceKey: 'team_id'
+});
+PlayerModel.belongsTo(TeamModel, {
+  foreignKey: 'team_id',
+  targetKey: 'team_id'
+});
+
+// db 객체에 할당
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+// db = { sequelize:sequelize. Sequelize : Sequelize }
+
+db.Player = PlayerModel;
+db.Profile = ProfileModel;
+db.Team = TeamModel;
+// db = { sequelize:sequelize. Sequelize : Sequelize, Player :PlayerModel, Profile: ProfileModel,Team:TeamModel }
+
+module.exports = db;
