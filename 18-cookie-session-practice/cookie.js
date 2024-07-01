14e6587aeca4fd3b341ac0e5310f23a77ad778c6
DAIN302 <dbgnfk12@gmail.com>
2024-07-01 18:55:31 +0900

    study :cookie & session

---
+++ 18-cookie-session-practice/cookie.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const app = express();
+const port = 8080;
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+const today = new Date()
+const expireDate = new Date()
+expireDate.setDate(today.getDate() + 1)
+
+// cookie-parser 미들웨어 
+app.use(cookieParser());
+const cookieConfig = {
+    httpOnly: true, 
+    expires: expireDate, 
+    signed: false,
+}
+
+app.get('/', (req, res) => {
+    res.render('cookie');
+});
+
+app.get('/setCookie', (req, res) => {
+    res.cookie('popup', 'hide', cookieConfig)
+    res.send(req.cookies)
+});
+
+app.listen(port, () => {
+    console.log(`Sever is running! The port number is ${port} ...`);
+})
