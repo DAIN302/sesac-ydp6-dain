a69d9e440c25bde2958fc698ffd7cbdd55d1f3de
DAIN302 <dbgnfk12@gmail.com>
2024-07-01 14:22:56 +0900

    study : 환경 변수

--- 19-env-variable/app.js
+++ 19-env-variable/app.js
@@ -2,6 +2,7 @@
 // console.log(ps); // object
 const express = require('express')
 const path = require('path')
+const dotenv = require('dotenv')
 const app = express()
 
 // dotenv 모듈을 이용해서 .env파일의 환경 변수를 불러옴
@@ -9,10 +10,13 @@ const app = express()
 // require('dotenv').config();
 
 // 환경변수 파일이 어디있는지 경로를 설정해줘야함
-require('dotenv').config({
-    // NODE_ENV에 따라서 적절한 .env 파일을 로드
+dotenv.config({
+    path: path.resolve(__dirname, `.env`),
+}); // 기본으로 .env파일 로드
+dotenv.config({
     path: path.resolve(__dirname, `.env.${process.env.NODE_ENV}`),
-});
+    override : true, // override 설정 (덮어쓰기)
+});// NODE_ENV에 따라서 적절한 .env 파일을 로드 (.env.develpment or .env.production)
 
 // process.env 객체를 통해 환경 변수에 접근
 const port = process.env.PORT || 8888;
