6699ce872145797e1d3f2aaba7a3e5a6e452f686
DAIN302 <dbgnfk12@gmail.com>
2024-07-08 14:22:22 +0900

    study : socket

--- 22-socketio_room/server.js
+++ 22-socketio_room/server.js
@@ -61,28 +61,67 @@ function getUserList(room) {
     return users; // 사용자 정보 배열값 반환
 }
 
+// 5. 채팅방 리스트
+// 채팅방 목록 초기화
+const roomList = [];
+
+// 함수랑 배열을 바깥에 선언한 이유 => 흐름이 막히지 않게 하기 위해 그래서 바깥에 미리 선언 (재사용성과 흐름을 위해서)
+
 io.on('connection', (socket)=>{
     // socket : 접속한 웹 브라우저 -> 브라우저에 접속하면 고유 id값 생성 : socket.id
     // io : 접속해 있는 모든 웹 브라우저
     console.log('접속 완', socket.id);
-    socket.on('create', (res)=>{
+
+    // 5. 채팅방 목록 미리보기 -> 맨 처음 접속했을 때 보기
+    io.emit('roomList', roomList)
+
+    // 채팅방 만들기
+    socket.on('create', (res)=>{ // 클라이언트에서 보내준 객체 데이터 -> {roomName : '', userName : ''}
         // console.log(res);
-        // join(방제목) -> '방제목'이 없으면 생성, 있으면 입장
-        socket.join(res.roomName); 
+        // join(방제목) -> '방제목'이 없으면 생성 후 방 입장, 있으면 그냥 그 방으로 입장
+        socket.join(res.roomName);  
         // console.log('방 생성 후', socket.rooms);
 
         // 사용자 정보 저장 
-        socket.roomName = res.roomName; // socket 객체에 없는 메소드 생성
+        // socket 객체에 없는 메소드 생성(roomName, userName)하고 값 할당
+        socket.roomName = res.roomName; 
         socket.userName = res.userName; 
 
         // 3. 공지 알림
         // 나를 제외한 모든 방 사람들에게 메세지 전달 
+        // 본인이 들어가는 입장 알림이라 본인을 제외하고 방 사람들에게 알림이 감
+        // 본인이 방에 있으면 그게 알림이 뜬다는데? -> 내가 있을때도 알림안떳으면 좋갰어 => broadcast쓰셈
         socket.to(res.roomName).emit('notice', `${socket.userName}님이 입장하셨습니다.`)
 
         // 4. 유저 리스트 갱신
         const userList = getUserList(res.roomName)
-        console.log('유저들아',userList);
+        // console.log('유저들아',userList);
         io.to(res.roomName).emit('userList', userList)
+
+        // 5. 채팅방 목록 갱신
+        if(!roomList.includes(res.roomName)){ // 중복이 아니면 추가
+            // 추가
+            roomList.push(res.roomName)
+            // 갱신된 목록
+            console.log('방목록',roomList);
+            io.emit('roomList', roomList); // 중복된 이름이 아니면 채팅방 생성 후 채팅방 목록 갱신
+        }
+    })
+
+    // 6. 메시지 전송
+    socket.on('sendMessage', (res)=>{ // res: {message : chatMesssage.value, user : myName, select : user.value}
+        console.log('sendMessage', res);
+        const { message, user, select } = res // 구조 분해 할당
+        if(select === 'all'){ 
+            // 특정방의 전체에게 메시지 전달
+            io.to(socket.roomName).emit('newMessage', {message, user, dm : false})
+        } else {
+            // 특정방의 dm대상자에게 메시지 전달
+            io.to(select).emit('newMessage', {message, user, dm : true})
+            // 자기 자신에게 메시지 보내기 (나한테서 보이기)
+            socket.emit('newMessage', {message, user, dm : true})
+        }
+
     })
 
 
