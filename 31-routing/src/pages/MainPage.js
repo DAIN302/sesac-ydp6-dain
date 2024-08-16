a9e27bac7edd29e0cef2de9d4243a3be987455d0
DAIN302 <dbgnfk12@gmail.com>
2024-08-16 18:51:55 +0900

    study : react router

--- 31-routing/src/pages/MainPage.js
+++ 31-routing/src/pages/MainPage.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 export default function MainPage() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  // console.log(searchParams.get('mode'));
+
+
   return (
-    <div>
-        <h1>Main Page</h1>
-        
+    // <div className={['Main', searchParams.get('mode')].join(' ')}>
+    <div className={`Main ${searchParams.get('mode') || ''}`}>
+        <h1>Main Page</h1>        
+        <button onClick={()=>{setSearchParams({mode : 'Dark'})}}>Dark Mode</button>
     </div>
   )
 }
+
+
+/*
+  코드 리뷰
+  useSearchParams() 훅은 현재 URL의 쿼리 파라미터를 읽고 수정할 수 있는 기능 제공
+
+  searchParams는 URLSearchParams 객체로, 현재 URL의 쿼리 파라미터를 나타냄
+  setSearchParams 는 쿼리 파라미터를 수정하는데 사용되는 함수
+
+  searchParams.get('mode') 는 현재 URL의 쿼리 스트링에서 mode라는 이름의 파라미터 값을 가져옴
+
+  버튼 클릭 이벤트 핸들러는 setSearchParams({mode : 'Dark'})를 호출하여 쿼리 스트링의 mode 파라미터를 'Dark'로 설정
+  이로 인해 URL이 업데이트 되며, 페이지가 새로고침 되지 않고 URL의 쿼리 파라미터가 변경됨
+
+*/
--- 31-routing/src/pages/ProductDetailPage.js
+++ 31-routing/src/pages/ProductDetailPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { productInfos } from '../components/ProductList';
 
 export default function ProductDetailPage() {
     const { productId } = useParams();
     // console.log(productId);
+
+    const navigate = useNavigate();
 
     const targetProduct =  productInfos[Number(productId) - 1]
     // productId의 type 이 string이기 떄문에 number로 형변환
@@ -14,6 +16,8 @@ export default function ProductDetailPage() {
   return (
     <div>
         <h1>ProductDetailPage</h1>
+        <button onClick={()=>navigate(-1)}>뒤로가기</button>
+        <button onClick={()=>navigate('/')}>홈으로 이동</button>
         <ul>
             <li>상품번호 :{id}</li>
             <li>상품명 : {title} </li>
@@ -23,3 +27,14 @@ export default function ProductDetailPage() {
     </div>
   )
 }
+
+/*
+    navigate() 숫자 인자 의미
+    양수 숫자('n')
+    - 현재 페이지에서 'n' 페이지 앞으로 이동
+    ex) 1 = 다음 페이지
+    
+    음수 숫자('-n')
+    - 현재 페이지에서 'n' 페이지 뒤로 이동
+    ex) -1 = 이전 페이지
+*/
