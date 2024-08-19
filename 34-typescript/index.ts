7ab49fe73ee2b5d7c011c129d8ae6ad055fc6574
DAIN302 <dbgnfk12@gmail.com>
2024-08-19 18:57:00 +0900

    study : typescript 맛보기

---
+++ 34-typescript/index.ts
@@ -0,0 +1,13 @@
+console.log('hihi');
+
+const msg:string = '타입 스크립트 배우기'
+const num:number = 100;
+console.log(msg);
+console.log(num);
+
+// index.js 로 컴파일링 해줘야함 
+// npx tsc index.js (npx tsc 파일 이름)
+
+// -> 귀찮아
+// npm i ts-node
+// npx ts-node index.ts -> 바로 실행해줌!
